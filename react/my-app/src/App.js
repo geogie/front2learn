7d46e7161db4915dd3a0b1291bc53f236a3f786d
geogie <geogie.ren@firstleap.cn>
2017-07-18 14:55:07 +0800

    react

---
+++ react/my-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>抠脚</h1>
+          <h2>Welcome 菜鸟</h2>
+          <p data-myattribute = "somevalue">这是一个很不错的 JavaScript 库!</p>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
