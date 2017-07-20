b9089a95f7795ca2a4dff82cd653a6186e12956a
geogie <geogie.ren@firstleap.cn>
2017-07-20 18:00:40 +0800

    react 掘金标题实现

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from "./components/Index/Header";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+export default class App extends Component {
+
+  componentDidMount() {
+    console.log(this.props.users)
+  }
+
+  render(){
+    return(
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>抠脚</h1>
-          <h2>Welcome 菜鸟</h2>
-          <p data-myattribute = "somevalue">这是一个很不错的 JavaScript 库!</p>
+          <Header/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-body">
+          <div className="welcome-view">
+            <div className="category-nav">
+              <div>category-nav</div>
+            </div>
+            <div className="main">main</div>
+            <div className="sidebar">sidebar</div>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
-
-export default App;
